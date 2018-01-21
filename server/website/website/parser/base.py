1e4cb69d71c01fd31b1d1d6e5e3663757f00832a
Shuli Jiang <11hifish@gmail.com>
2018-01-20 22:53:51 -0500

    test upload

--- server/website/website/parser/base.py
+++ server/website/website/parser/base.py
@@ -91,16 +91,18 @@ class BaseParser(object):
                 conv_value = self.convert_enum(value, metadata)
             elif metadata.vartype == VarType.INTEGER:
                 conv_value = self.convert_integer(value, metadata)
-                exn_str = 'maxval: ' + str(KnobCatalog.maxval) + 'minval: ' + str(KnobCatalog.minval) + 'conv_val: ' + str(conv_value)
                 if not self._check_knob_num_in_range(conv_value):
-                    # raise Exception('Knob num value not in range!')
-                    raise Exception(exn_str)
+                    print(KnobCatalog.maxval)
+                    print(KnobCatalog.minval)
+                    print(conv_value)
+                    raise Exception('Knob num value not in range!')
             elif metadata.vartype == VarType.REAL:
                 conv_value = self.convert_real(value, metadata)
-                exn_str = 'maxval: ' + str(KnobCatalog.maxval) + 'minval: ' + str(KnobCatalog.minval) + 'conv_val: ' + str(conv_value)
                 if not self._check_knob_num_in_range(conv_value):
-                    # raise Exception('Knob num value not in range!')
-                    raise Exception(exn_str)
+                    print(KnobCatalog.maxval)
+                    print(KnobCatalog.minval)
+                    print(conv_value)
+                    raise Exception('Knob num value not in range!')
             elif metadata.vartype == VarType.STRING:
                 conv_value = self.convert_string(value, metadata)
             elif metadata.vartype == VarType.TIMESTAMP:
